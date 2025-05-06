b46234f40f291b83e33b573c18c696cefc150094
AevitasDragonkin <aevitas.dragonkin@gmail.com>
2025-05-06 21:09:37 +0200

    Provided helper function to generate enemies

--- src/modules/battleTree/BattleTreeController.ts
+++ src/modules/battleTree/BattleTreeController.ts
@@ -1,3 +1,24 @@
-export class BattleTreeController {
+import { BattleTreePokemon } from './BattleTreePokemon';
+import { BattleTreeRand } from './BattleTreeRand';
 
+export class BattleTreeController {
+    public static getRandomTeamForStage(seed: number, stage: number, amount: number): BattleTreePokemon[] {
+        // TODO : BT : Return actual randomized list from all pokemon
+        const list: BattleTreePokemon[] = [
+            new BattleTreePokemon({ name: 'Bulbasaur', level: this.calculatePokemonLevelForStage(stage) }),
+            new BattleTreePokemon({ name: 'Squirtle', level: this.calculatePokemonLevelForStage(stage) }),
+            new BattleTreePokemon({ name: 'Charmander', level: this.calculatePokemonLevelForStage(stage) }),
+            new BattleTreePokemon({ name: 'Mew', level: this.calculatePokemonLevelForStage(stage) }),
+            new BattleTreePokemon({ name: 'Mewtwo', level: this.calculatePokemonLevelForStage(stage) }),
+            new BattleTreePokemon({ name: 'Rhydon', level: this.calculatePokemonLevelForStage(stage) }),
+        ];
+
+        BattleTreeRand.seed(seed + stage);
+        return BattleTreeRand.shuffleArray(list).slice(0, amount);
+    }
+
+    public static calculatePokemonLevelForStage(stage: number): number {
+        // TODO : BT : Calculate proper level for provided stage
+        return stage;
+    }
 }
